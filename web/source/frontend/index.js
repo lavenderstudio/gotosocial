ea7e3c0cd7faa32bec6727103b3881fdd2c0cdb2
Jade Arson <codeberg@wolfyja.de>
2026-02-26 11:34:51 +0100

    [feature/frontend] add Träwelling status to profile (#4540)
    # Description
    
    > If this is a code change, please include a summary of what you've coded, and link to the issue(s) it closes/implements.
    >
    > If this is a documentation change, please briefly describe what you've changed and why.
    
    This pull request implements a feature to show one's Träwelling status in place of a plain URL, whenever that mentioned user is checked in to a currently-ongoing journey.
    
    ![A check-in into an underground train in Berlin.](/attachments/de3d8e55-14a4-4a6e-a388-dacdcf6fe669)
    
    ![A check-in into a ferry in Bad Schandau.](/attachments/61f59877-bc17-465c-af2e-e788a6a30765)
    
    ![A check-in into a tram replacement service run by a bus in Leipzig.](/attachments/2d80a4e7-c2d8-4c56-89c4-97c374af1a4b)
    
    ![A check-in into a train to Munich.](/attachments/9b5f931f-0ade-4512-a46c-996c50dac294)
    
    (this isn't the final version *yet*, but we're getting quite close -- at least in my opinion :3)
    (all versions attached solely to show that the icon does actually change across services, depending on its category/mode.)
    
    ## Checklist
    
    Please put an x inside each checkbox to indicate that you've read and followed it: `[ ]` -> `[x]`
    
    If this is a documentation change, only the first checkbox must be filled (you can delete the others if you want).
    
    - [X] I/we have read the [GoToSocial contribution guidelines](https://codeberg.org/superseriousbusiness/gotosocial/src/branch/main/CONTRIBUTING.md).
    - [ ] I/we have discussed the proposed changes already, either in an issue on the repository, or in the Matrix chat.
    - [X] I/we have not leveraged AI to create the proposed changes.
    - [X] I/we have performed a self-review of added code.
    - [X] I/we have written code that is legible and maintainable by others. (← hopefully? ><)
    - [X] I/we have commented the added code, particularly in hard-to-understand areas.
    - [ ] I/we have made any necessary changes to documentation. (← not yet, but will do so once it's in a state where I consider it Good Enough)
    - [ ] I/we have added tests that cover new code. (← not yet, I'd really appreciate some help in that regard!)
    - [X] I/we have run tests and they pass locally with the changes. (← ...if re-adding the assets to the testrig counts?)
    - [ ] I/we have run `go fmt ./...` and `golangci-lint run`.
    
    Co-authored-by: ⛧-128293 [jade arson.] <awoo@wolfyja.de>
    Reviewed-on: https://codeberg.org/superseriousbusiness/gotosocial/pulls/4540
    Co-authored-by: Jade Arson <codeberg@wolfyja.de>
    Co-committed-by: Jade Arson <codeberg@wolfyja.de>

--- web/source/frontend/index.js
+++ web/source/frontend/index.js
@@ -434,3 +434,322 @@ document.querySelectorAll("div#profile-fields dl div.field").forEach((field) => 
 		console.error(error.message);
 	}
 });
+
+// Scan for the first Träwelling profile field and replace
+// its value with the currently boarded service, if available.
+//
+// Träwelling usernames seem to follow a similar structure to
+// how Twitter did it back in the day (considering that Träwelling
+// started out as a platform linked to Twitter and building on top
+// of it), so parsing usernames this way should be fine…?
+// (feel free to correct me on any of this, by the way!!)
+//
+// See:
+//
+// - https://traewelling.de/api/documentation
+const traewellingRe = new RegExp(/^https:\/\/traewelling\.de\/@([^/]+)$/, "u");
+let calledTraewelling = false;
+document.querySelectorAll("div#profile-fields dl div.field").forEach((field) => {
+	// If we called Träwelling once
+	// already this page load, bail.
+	if (calledTraewelling) {
+		return;
+	}
+
+	const k = field.querySelector("dt");
+	if (!k) {
+		// No <dt> inside this
+		// field? Weird but OK.
+		return;
+	}
+
+	const kText = k.textContent;
+	if (kText === null) {
+		// Also strange but
+		// let's just bail.
+		return;
+	}
+	
+	// Check if key == "Träwelling" or == "Traewelling" (case insensitive).
+	// TODO: add handling of "Traewelling"
+	if (kText.localeCompare("Träwelling", undefined, { sensitivity: "base" }) !== 0) {
+		// Not interested.
+		return;
+	}
+
+	// Get the value.
+	const v = field.querySelector("dd");
+	if (!v) {
+		// No <dd> inside this
+		// field? Weird but OK.
+		return;
+	}
+
+	// Look for an <a> tag inside the <dd>.
+	const oldAs = v.getElementsByTagName("a");
+	if (oldAs.length !== 1) {
+		// Nothing
+		// in here.
+		return;
+	}
+
+	const oldA = oldAs[0];
+	const profileURL = oldA.textContent;
+	if (!profileURL) {
+		// Also strange but
+		// let's just bail.
+		return;
+	}
+
+	// We're looking for a Träwelling URL.
+	const match = profileURL.match(traewellingRe);
+	if (match.length !== 2) {
+		// Not a match.
+		return;
+	}
+	const trUsername = match[1];
+	
+	try {
+		const apiURL = encodeURI(`https://traewelling.de/api/v1/user/${trUsername}/statuses`);
+		fetch(apiURL).then(res => {
+			// Mark that we
+			// called Träwelling already.
+			calledTraewelling = true;
+			
+			// Check result...
+			if (!res.ok) {
+				throw new Error(`Response status: ${res.status}`);
+			}
+			return res.json();
+		}).then(json => {
+			let foundCurrentlyValidJourney = false;
+			let currentlyProcessedJourney = 0;
+			while(currentlyProcessedJourney < 15) {
+				// If we already found a valid journey at some point down the code,
+				// break out of the while loop here.
+				if(foundCurrentlyValidJourney === true) {break;}
+				// Parse out the object.
+				const payload = json.data[currentlyProcessedJourney];
+				currentlyProcessedJourney++;
+				if (!payload) {
+					// Can't do anything
+					// with no payload.
+					break;
+				}
+
+				const currentJourney = payload.train;
+				if (!currentJourney) {
+					// Can't do anything
+					// with no journeys.
+					break;
+				}
+
+				const lineName = currentJourney.lineName;
+				if (!lineName) {
+					// Can't do anything
+					// with no line name, really.
+					continue;
+				}
+
+				// this will be useful at some later point :3
+				const trainType = currentJourney.category;
+
+				const origin = currentJourney.origin.name;
+				const destination = currentJourney.destination.name;
+				if (origin === undefined || destination === undefined) {
+					// Can't display this train.
+					continue;
+				}
+
+				// TODO: We're pulling in the origin's departure time and station name,
+				//       but we still don't do anything with it (yet).
+				const realDeparture = currentJourney.origin.departureReal;
+				const scheduledDeparture = currentJourney.origin.departurePlanned;
+				const manualDeparture = currentJourney.manualDeparture;
+				const realArrival = currentJourney.destination.arrivalReal;
+				const scheduledArrival = currentJourney.destination.arrivalPlanned;
+				const manualArrival = currentJourney.manualArrival;
+				if(payload.event) { var eventHashtag = payload.event.hashtag; }
+				let departure, arrival;
+				let /*departureType,*/ arrivalType;
+				
+				// Träwelling implements the priority of time info this way:
+				// manual > real-time > scheduled.
+
+				// Check if there *isn't* a manual departure time…
+				if (manualDeparture === null) {
+					// …and if there isn't even a real-time departure time…
+					if(realDeparture === null) {
+						// …then we'll take the scheduled departure.
+						departure = scheduledDeparture;
+						// departureType = "scheduled";
+					} else {
+						// else we'll use the real-time departure.
+						departure = realDeparture;
+						// departureType = "realtime";
+					}
+				} else {
+					// though ideally we'd like to use the manually entered departure time.
+					departure = manualDeparture;
+					// departureType = "manual";
+				}
+				
+
+				// Same logic as above, but find-and-replace departure with arrival.
+				if (manualArrival === null) {
+					if(realArrival === null) {
+						arrival = scheduledArrival;
+						arrivalType = "scheduled";
+					} else {
+						arrival = realArrival;
+						arrivalType = "realtime";
+					}
+				} else {
+					arrival = manualArrival;
+					arrivalType = "manual";
+				}
+
+				// Check if the browser's local time is within
+				// the specified (most recent) check-in.
+				const currentTime = new Date();
+
+				if(currentTime < new Date(departure)) {
+					// Can't work with check-ins before the departure time.
+					continue;
+				}
+
+				if(currentTime > new Date(arrival)) {
+					// Can't work with check-ins past the arrival time.
+					continue;
+				}
+
+				// Time to do some date() shenanigans. :3
+				// (yes, the pun was intended.)
+				//
+				// TODO: clean up this mess lmfao
+				const formattedArrival = new Intl.DateTimeFormat(undefined, {hour: '2-digit', minute: '2-digit', hour12: false}).format(new Date(arrival));
+				const formattedScheduledArrival = new Intl.DateTimeFormat(undefined, {hour: '2-digit', minute: '2-digit', hour12: false}).format(new Date(currentJourney.destination.arrivalPlanned));
+				//const formattedDeparture = new Intl.DateTimeFormat(undefined, {hour: '2-digit', minute: '2-digit', hour12: false}).format(new Date(departure));
+				//const formattedScheduledDeparture = new Intl.DateTimeFormat(undefined, {hour: '2-digit', minute: '2-digit', hour12: false}).format(new Date(currentJourney.destination.departurePlanned));
+
+				// We can actually work with this now.
+				//
+				// Rewrite the existing <dd> with the
+				// currently boarded train, and keep the
+				// link to the user's Träwelling profile.
+				const vNew = document.createElement("dd");
+				
+				// cute little public transit icons!
+				const i = document.createElement("i");
+				i.ariaHidden = "true";
+				// see, I said it's going to be useful later!
+				switch (trainType) {
+					case "regional":         // RE, RB
+					case "regionalExp":      // IC
+					case "tram":             // trams (hopefully self-explanatory)
+					case "nationalExpress":  // FlixTrain (so far)
+					case "suburban":         // S-Bahn
+						i.className = "fa fa-fw fa-train";
+						break;
+					case "subway":           // U-Bahn
+						i.className = "fa fa-fw fa-subway";
+						break;
+					case "ferry":            // self-explanatory
+						i.className = "fa fa-fw fa-ship";
+						break;
+					case "bus":              // self-explanatory
+						i.className = "fa fa-fw fa-bus";
+						break;
+					default:                 // for when there's no good match!
+						i.className = "fa fa-fw fa-train";
+				}
+				vNew.appendChild(i);
+
+				// Add some whitespace between the icon and the following text.
+				vNew.appendChild(document.createTextNode(" "));
+
+				// Build a new link, taking the href from the old link.
+				const a = document.createElement("a");
+				a.href = oldA.href;
+				a.rel = "nofollow noreferrer noopener";
+				a.target = "_blank";
+
+				// Add user's display name to the original link in bold.
+				const displayNameE = document.createElement("b");
+				displayNameE.textContent = `${payload.userDetails.displayName}`;
+				a.appendChild(displayNameE);
+				vNew.appendChild(a);
+
+				// probably added the träwelling user's display name in the jankiest way possible
+				vNew.appendChild(document.createTextNode(" is currently on the "));
+
+				// Build a brand-new link, taking
+				// the href from the check-in ID.
+				const aStatus = document.createElement("a");
+				aStatus.href = `https://traewelling.de/status/${payload.id}`;
+				aStatus.rel = "nofollow noreferrer noopener";
+				aStatus.target = "_blank";
+
+				// Add train name in bold.
+				const trainNameE = document.createElement("b");
+				trainNameE.textContent = lineName;
+				aStatus.appendChild(trainNameE);
+
+				// Add joiner in normal font.
+				aStatus.appendChild(document.createTextNode(" service to "));
+				
+				// Add destination name in bold.
+				const destinationNameE = document.createElement("b");
+				destinationNameE.textContent = destination;
+				aStatus.appendChild(destinationNameE);
+
+				// Add event-related hashtag (if available), in normal font.
+				if(eventHashtag !== null && eventHashtag !== undefined) {
+					aStatus.appendChild(document.createTextNode(` for \#${eventHashtag}`));
+				}
+
+				// Add yet another joiner in normal font.
+				// Here, we're checking if it's a manual/scheduled/real-time arrival.
+				switch(arrivalType) {
+					case "manual":   // This is for when it's an arrival time has been manually entered by the user.
+						aStatus.appendChild(document.createTextNode(", manually told to arrive at "));
+						break;
+					case "realtime": // This is for when it's an arrival time with real-time info.
+						aStatus.appendChild(document.createTextNode(", arriving at "));
+						break;
+					case "scheduled": // And this is for when it's an arrival time only with scheduled info.
+						aStatus.appendChild(document.createTextNode(", scheduled to arrive at "));
+						break;
+				}
+				
+				// Add arrival time in bold.
+				// TODO: consider adding the difference between scheduled and real times?
+				//       (that is, if there's real-time data available.)
+				const arrivalTimeE = document.createElement("b");
+				arrivalTimeE.textContent = formattedArrival;
+				aStatus.appendChild(arrivalTimeE);
+				// If there's anything other than scheduled info available, *and*
+				// if the scheduled info isn't the same as the other info, display
+				// the scheduled arrival time as well, but in a separate element.
+				if(arrivalType !== "scheduled" && formattedArrival !== formattedScheduledArrival) {
+					const scheduledArrivalTimeE = document.createElement("i");
+					scheduledArrivalTimeE.textContent = ` (scheduled arrival at ${formattedScheduledArrival})`;
+					aStatus.appendChild(scheduledArrivalTimeE);
+				}
+
+				// Put the link
+				// in the definish.
+				vNew.appendChild(aStatus);
+
+				// Do the replacement.
+				field.replaceChild(vNew, v);
+
+				// Tell the variable that we found a valid journey.
+				foundCurrentlyValidJourney = true;
+			}
+		});
+	} catch (error) {
+		// eslint-disable-next-line no-console
+		console.error(error.message);
+	}
+});
